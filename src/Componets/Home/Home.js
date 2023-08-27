a4bcf64b990832265f8a3e8192a9f5e115667e04
Arun Rajak <arunrajak527@gmail.com>
2023-08-27 19:31:07 +0530

    remove login routes add screen on home

--- src/Componets/Home/Home.js
+++ src/Componets/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import LoginPage from '../LoginPage/LoginPage'
+import RecordScreen from '../RecordingMedia/RecordScreen'
 
 const Home = () => {
   return (
     <div>
       <LoginPage></LoginPage>
+      <RecordScreen></RecordScreen>
     </div>
   )
 }
